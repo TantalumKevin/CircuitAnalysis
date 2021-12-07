6cd12a94296e86f6893e2abdf69a67aee56e27a8
kevin_ud <1308098049@qq.com>
2021-12-07 12:53:10 +0800

    已写好支路class&修改了小细节

--- main.py
+++ main.py
@@ -84,12 +84,11 @@ class branch():
                                 temp.append(Z_temp)
                                 break
 
-                
-  
 
 print("请注意本程序\033[0;30;47m仅\033[0m可处理如图所示拓扑结构电路（支路编号、方向及结点编号已给出）")
 time.sleep(2)
 im1.show()
 
 for i in range(1,7):
+    #初始化支路类
     b.append(branch(i))
